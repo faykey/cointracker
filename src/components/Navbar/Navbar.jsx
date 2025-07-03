3c9ef670edde6e8d2b8c6059d8e6e7145ff5ea76
faykey <fikayomicheal4@gmail.com>
2025-07-03 01:53:30 +0100

    Initialize

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,54 @@
+import React, { useContext } from 'react';
+import './Navbar.css';
+import logo from '../../assets/logo.png';
+import arrow_icon from '../../assets/arrow_icon.png';
+import { CoinContext } from '../../context/CoinContext';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+
+    const { setCurrency } = useContext(CoinContext)
+    
+    const currencyHandler = (event) => {
+        switch (event.target.value) {
+            case "usd": {
+                setCurrency({ name: "usd", symbol: "$" });
+                break;
+            }
+            case "eur": {
+                setCurrency({ name: "eur", symbol: "€" });
+                break;
+            }
+            case "ngn": {
+                setCurrency({ name: "ngn", symbol: "₦" });
+                break;
+            }
+            default : {
+                setCurrency({ name: "usd", symbol: "$" });
+                break;
+            }
+        }
+    }
+
+  return (
+      <div className='navbar'>
+          <Link to={`/`}>
+            <img src={logo} alt='logo' className='logo' />
+          </Link>
+          
+          <ul>
+              <Link to={`/`}><li>HOME</li></Link>
+              <li>NEWS</li>
+          </ul>
+          <div className='nav-right'>
+              <select onChange={currencyHandler}>
+                  <option value="usd">USD</option>
+                  <option value="eur">EUR</option>
+                  <option value="ngn">NGN</option>
+              </select>
+          </div>
+    </div>
+  )
+}
+ 
+export default Navbar
